583203ee0e16516c69d32cd32f07e64a1fcbcac5
khangNguyen18 <minhkhang072003@gmail.com>
2024-06-28 22:41:56 +0700

    create register screen

---
+++ lib/screens/auth/register_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('đăng ký nè'),
+    );
+  }
+}
